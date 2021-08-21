a7d0b5e12547c854c65bdf2b294cac1b40863d13
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-21 18:28:53 +0200

    Rename project folders

--- website/settings.py
+++ website/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'callback_form',
     'cs_forms',
+    
 ]
 
 MIDDLEWARE = [
