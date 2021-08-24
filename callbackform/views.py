961de99ba7b7ca4d159a24852a8faa607c60c9e5
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-24 17:15:51 +0200

    Render an app template

--- callbackform/views.py
+++ callbackform/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 def print_callback_form(request):
     """The home page for callbackform."""
-    return render(request, 'index.html')
+    return render(request, 'callbackform/index.html')
