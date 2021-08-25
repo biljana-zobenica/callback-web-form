546796b329c3b06dd0583cfb2fe21f2d96004c7d
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-25 16:27:58 +0200

    Correct not required field to be set that way

--- callbackform/forms.py
+++ callbackform/forms.py
@@ -4,8 +4,8 @@ from django import forms
 
 class CallbackForm(forms.ModelForm):
         name = forms.CharField(max_length= 50, required= True, widget=forms.TextInput(attrs={'class':'form-control'}))
-        phone_number = forms.CharField(max_length= 50, required= True, widget=forms.TextInput(attrs={'class':'form-control'}))
-        company = forms.CharField(max_length= 70, required= True, widget=forms.TextInput(attrs={'class':'form-control'}))
+        phone_number = forms.CharField(max_length= 50,required= False, widget=forms.TextInput(attrs={'class':'form-control'}))
+        company = forms.CharField(max_length= 70,required= False, widget=forms.TextInput(attrs={'class':'form-control'}))
         email = forms.EmailField(max_length= 50, required= True, widget=forms.EmailInput(attrs={'class':'form-control'}))
         subject = forms.CharField(max_length= 200, required= True, widget=forms.TextInput(attrs={'class':'form-control'}))
         problem_description = forms.CharField(max_length= 1000, required= True, widget=forms.TextInput(attrs={'class':'form-control'}))
