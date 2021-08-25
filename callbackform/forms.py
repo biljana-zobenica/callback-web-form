80355f090fff95c9b5431f6e446321eb524e4e63
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-25 15:29:38 +0200

    Add bootstrap style to form

--- callbackform/forms.py
+++ callbackform/forms.py
@@ -1,8 +1,15 @@
 from django.forms import ModelForm
 from .models import Callback
+from django import forms
 
-class CallbackForm(ModelForm):
-    class Meta:
-        model = Callback
-        fields = ('name', 'phone_number', 'company', 'email', 'subject', 
-                  'problem_description',)
+class CallbackForm(forms.ModelForm):
+        name = forms.CharField(max_length= 50, required= True, widget=forms.TextInput(attrs={'class':'form-control'}))
+        phone_number = forms.CharField(max_length= 50, required= True, widget=forms.TextInput(attrs={'class':'form-control'}))
+        company = forms.CharField(max_length= 70, required= True, widget=forms.TextInput(attrs={'class':'form-control'}))
+        email = forms.EmailField(max_length= 50, required= True, widget=forms.EmailInput(attrs={'class':'form-control'}))
+        subject = forms.CharField(max_length= 200, required= True, widget=forms.TextInput(attrs={'class':'form-control'}))
+        problem_description = forms.CharField(max_length= 1000, required= True, widget=forms.TextInput(attrs={'class':'form-control'}))
+
+        class Meta:
+            model = Callback
+            fields = ('name', 'phone_number', 'company', 'email', 'subject', 'problem_description')
