0ed1e4e17a6bbcf749a22357f77ea8d6e79ebb66
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-26 12:01:17 +0200

    Fix admin archived option description typo

--- callbackform/admin.py
+++ callbackform/admin.py
@@ -4,14 +4,14 @@ from .models import Callback
 
 # Register your models here.
 
-@admin.action(description='Mark selected forms as archieved')
-def make_archieved(modeladmin, request, queryset):
+@admin.action(description='Mark selected forms as archived')
+def make_archived(modeladmin, request, queryset):
     queryset.update(status='a')
 
 class CallbackAdmin (admin.ModelAdmin):
     list_display = ('subject', 'name', 'comment', 'status', 'submitted_date_time', 'support_date_time')
     list_filter = ('submitted_date_time', 'support_date_time')
-    actions = [make_archieved]
+    actions = [make_archived]
 
     def has_delete_permission(self, request, obj=None):
         return False
